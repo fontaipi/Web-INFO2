169d6922eb2fc1a273e5ce7f1acdf4433695d3cc
QuentinR <quentin.reynaud99@gmail.com>
2020-01-29 15:57:30 +0100

    Troisieme test : test capitalisation lors de la creation d'un identifiant d'une personne

--- tests/PersonneTest.php
+++ tests/PersonneTest.php
@@ -13,11 +13,20 @@ class PersonneTest extends TestCase
         );
     }
 
-    public function testCanBeCreatedFromConcatenationParameters(): void
+    public function testIdPersonneConcatenationParameters(): void
     {
         $personne = new Personne('dupont','heliot',new DateTime('2010-02-21'));
         $this->assertEquals(
             "dupontheliot2010-02-21",
+            $personne->getId()
+        );
+    }
+
+    public function testIdPersonneCapitalizeParameters(): void
+    {
+        $personne = new Personne('Dupont','Heliot',new DateTime('2010-02-21'));
+        $this->assertEquals(
+            "DupontHeliot2010-02-21",
             $personne->getId()
         );
     }
