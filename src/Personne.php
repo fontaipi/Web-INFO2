fc64eaccf277c065b4eeff12e2465b689c39eecc
fontaipi <pierrefontaine00@gmail.com>
2020-01-29 15:38:56 +0100

    Premier test OK : class Personne crée

---
+++ src/Personne.php
@@ -0,0 +1,16 @@
+<?php
+
+final class Personne
+{
+
+    private $nom;
+    private $prenom;
+    private $date;
+
+    public function __construct(string $nom, string $prenom, DateTime $date)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->date = $date;
+    }
+}
